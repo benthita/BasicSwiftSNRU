7e9fda91d862d4487608804c2ef531b9987a8cdb
Pring123 <benbentitayenbamrung@gmail.com>
2019-03-18 14:28:14 +0700

    Start Data Type

---
+++ DataTypeOfVariable.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//String Type ตัวแปลที่เป็นอักษร
+
+var myName = "Doramon" //นี่คือการประกาศตัวแปรที่อาศัยชนิดของ Valu บอก Data Type
+var mySurname: String = "AAAAAA" //นี่คือการประกาศแบบกำหนด Data Type ตั้งแต่เกิด
+
+
---
+++ VarialbleAndContant.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//Explicit Variabie
+//การประกาศตัวแปร
+
+var myName =  "Pring"
+myName = "Doramon"
+
+//Explicit Constant
+let myFriend = "Nobita"
+//myFriend = "Sunako"Error เพราะ myFriend เป็นคำคงที่ Let
+
